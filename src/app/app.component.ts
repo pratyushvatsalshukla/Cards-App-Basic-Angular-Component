ac9d080420032b9f012d971bd7833ab0a95adfd6
pratyushvatsalshukla <pratyushshukla9333@gmail.com>
2021-07-04 02:39:04 +0530

    Learnt Using Components and ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards-app';
+  posts = [
+      { title : "Neat Tree", 
+        imageUrl : 'assets/tree.jpeg',
+        username : 'nature',
+        content :  'I saw this neat tree today'
+    },
+    { title : "Anowy Mountain", 
+        imageUrl : 'assets/mountain.jpeg',
+        username : 'mountainlovers',
+        content :  'Here is a picture of a snowy mountain'
+    },
+    { title : "Mountain Biking", 
+        imageUrl : 'assets/biking.jpeg',
+        username : 'biking',
+        content :  'I did some biking today'
+    }
+  
+  ] ;
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
+  @Input() title ='';
+  @Input() imageUrl ='';
+  @Input() content ='';
+  @Input() username ='';
 
   constructor() { }
 
